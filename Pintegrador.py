3e40b6d3b0975cfb39bc538e3ec958585828ab09
= <jes.alberto_@live.com>
2020-09-10 15:56:41 -0500

    ultimo cambio

--- Pintegrador.py
+++ Pintegrador.py
@@ -1,20 +1,38 @@
 
 
-def __init__ (self):
-    cliente="no hay cliente"
+class Banco():
+
+    def __init__ (self):
+        self.cliente="no hay cliente"
+        opcion= 0
 
 
-def crearcliente(self):
-    cliente=input("Cual es su nombre")
-    return cliente
+    def crearcliente(self):
+       self.cliente=input("Cual es su nombre")
+        return cliente
 
-print("que accion va a realizar")
+    def IngreEfectivo(self):
+        self.efectivo=input("dinero a ingrear")
+        return efectivo
+
+    def 
+
+
+opcion=input("que accion va a realizar")
+
 print("1 Crear Cliente")
 print("2 ingresar Efectivo")
 print("3 retirar efectivo")
 print("4 elimiar cliente")
 print ("5 buscar Cliente")
 print("imprimir datos de cliente")
+
+objeto.Banco
+
+
+if opcion==1:
+    crearcliente()
+if opcion==2:
 
 
 
