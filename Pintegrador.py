f89469180297f34b31ab596869cd907b1772fb4b
Jesse <jes.alberto_@live.com>
2020-09-10 15:13:47 -0500

    Initial commit

---
+++ Pintegrador.py
@@ -0,0 +1,20 @@
+
+
+def __init__ (self):
+    cliente="no hay cliente"
+
+
+def crearcliente(self):
+    cliente=input("Cual es su nombre")
+    return cliente
+
+print("que accion va a realizar")
+print("1 Crear Cliente")
+print("2 ingresar Efectivo")
+print("3 retirar efectivo")
+print("4 elimiar cliente")
+print ("5 buscar Cliente")
+print("imprimir datos de cliente")
+
+
+
